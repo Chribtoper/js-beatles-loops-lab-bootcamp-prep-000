c4abcc7e5bd193986157094694081bf8294f9246
Chribtoper <xcstolfax@gmail.com>
2018-06-30 04:47:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments){
+  var myArray = []; 
+    for (let i = 0; i < musicians.length; i++){
+      myArray[i] = musicians[i] + ' plays ' + instruments[i];
+    }
+  return myArray; 
+}
+
+function johnLennonFacts(facts){
+  let i = 0;
+    while (i < facts.length){
+      facts[i] += '!!!';
+      i++;
+    }
+  return facts;
+}
+
+function iLoveTheBeatles(num){
+  var arr = [];
+  let i = 0;
+  do {
+  arr[i] = "I love the Beatles!";
+} while (i < 15){
+  i++;
+}
+}
+
